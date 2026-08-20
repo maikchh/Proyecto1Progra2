e1617ba3c2e390e73b7817b856bed5517305ff1c
christopher7F <christopher628283@gmail.com>
2026-08-19 19:59:20 -0600

    vista tareas

--- src/com/proyecto1/mvc/views/tareasCompletadasPendientes.java
+++ src/com/proyecto1/mvc/views/tareasCompletadasPendientes.java
@@ -6,9 +6,16 @@ import javax.swing.JTextField;
 import javax.swing.JComboBox;
 import java.awt.FlowLayout;
 import javax.swing.JLabel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class tareasCompletadasPendientes extends JPanel {
 	private JComboBox cbxCategTareaP;
+	private JTable table;
+	private final JButton btnEditar = new JButton("Editar");
 
 	/**
 	 * Create the panel.
@@ -30,8 +37,26 @@ public class tareasCompletadasPendientes extends JPanel {
 		cbxCategTareaP = new JComboBox();
 		panel_1.add(cbxCategTareaP);
 		
-		JLabel lblNewLabel = new JLabel("New label");
-		panel_1.add(lblNewLabel);
+		JPanel panel_2 = new JPanel();
+		panel.add(panel_2, BorderLayout.CENTER);
+		panel_2.setLayout(new BorderLayout(0, 0));
+		
+		JLabel lblCategoria_1 = new JLabel("Categoria:");
+		panel_2.add(lblCategoria_1, BorderLayout.NORTH);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		panel_2.add(scrollPane, BorderLayout.CENTER);
+		
+		table = new JTable();
+		scrollPane.setViewportView(table);
+		
+		JButton btnNueva = new JButton("Nueva");
+		panel_2.add(btnNueva, BorderLayout.SOUTH);
+		btnEditar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		panel_2.add(btnEditar, BorderLayout.SOUTH);
 
 	}
 
